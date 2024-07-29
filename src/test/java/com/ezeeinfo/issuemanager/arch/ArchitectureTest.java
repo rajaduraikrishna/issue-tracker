f62c7ae3ea646033cbcae1fbed558d5736fbb556
VijayWhat <vijayrv042@gmail.com>
2024-07-29 16:57:50 +0530

    architecture Test file completed

--- src/test/java/com/ezeeinfo/issuemanager/arch/ArchitectureTest.java
+++ src/test/java/com/ezeeinfo/issuemanager/arch/ArchitectureTest.java
@@ -1,5 +1,15 @@
 package com.ezeeinfo.issuemanager.arch;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.Test;
 
-public class ArchitectureTest {
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
+class ArchitectureTest {
+    @Test
+    void some_architecture_rule() {
+        JavaClasses serviceClasses = new ClassFileImporter().importPackages("com.ezeeinfo.issuemanager.service");
 
+        fields().that().arePrivate().should().beFinal()
+                .check(serviceClasses);
+    }
 }
